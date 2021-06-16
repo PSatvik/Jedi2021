1c70e2071297374c1ef1d8621b21717319740d1e
Satvik Pandey <satvikpandey492@gmail.com>
2021-06-16 23:37:42 +0530

    Adding separate folders

---
+++ CCode/helloWorld.c
@@ -0,0 +1,22 @@
+// Hello World
+
+#include<stdio.h>
+
+void helloworld()
+{
+
+}
+
+int sub(int x, int y)
+{
+	return x-y;
+}
+int main()
+{
+
+	printf("Hello World!");
+	printf("Awesome World");
+	return 0;
+
+}
+
